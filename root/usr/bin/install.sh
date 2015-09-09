1c54b6c5bedb5c5365bce68c6fdd0532bfce90cc
Avesh Agarwal <avagarwa@redhat.com>
2015-09-09 12:02:18 -0400

    Fixes install script with atomic install so that etcd container is created with required ports, otherwise atomic install followed by atomic run wont work as expected as the container ports wont be mapped to host ports.

--- root/usr/bin/install.sh
+++ root/usr/bin/install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Create Container
-chroot ${HOST} /usr/bin/docker create --name ${NAME} ${IMAGE}
+chroot ${HOST} /usr/bin/docker create -p 4001:4001 -p 7001:7001 -p 2379:2379 -p 2380:2380 --name ${NAME} ${IMAGE}
 
 # Install systemd unit file for running container
 sed -e "s/TEMPLATE/${NAME}/g" /etc/systemd/system/etcd_container_template.service > ${HOST}/etc/systemd/system/etcd_container_${NAME}.service
