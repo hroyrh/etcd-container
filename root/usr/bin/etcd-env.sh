76754722c335e25253cb4962d89c182a5e6fa918
Avesh Agarwal <avagarwa@redhat.com>
2015-09-11 03:59:22 -0400

    Fixed etcd-env shell script so that it does not pass ETCD_INITIAL_CLUSTER by default otherwise etcd cluster can not be boodstrapped with the discovery mechanisms.

--- root/usr/bin/etcd-env.sh
+++ root/usr/bin/etcd-env.sh
@@ -7,7 +7,7 @@ export ETCD_ADVERTISE_CLIENT_URLS=http://${ipaddress}:2379,http://${ipaddress}:4
 export ETCD_LISTEN_CLIENT_URLS=http://0.0.0.0:2379,http://0.0.0.0:4001
 export ETCD_INITIAL_ADVERTISE_PEER_URLS=http://${ipaddress}:2380,http://${ipaddress}:7001
 export ETCD_LISTEN_PEER_URLS=http://0.0.0.0:2380,http://0.0.0.0:7001
-export ETCD_INITIAL_CLUSTER=$HOSTNAME=http://${ipaddress}:2380,$HOSTNAME=http://${ipaddress}:7001
+#export ETCD_INITIAL_CLUSTER=$HOSTNAME=http://${ipaddress}:2380,$HOSTNAME=http://${ipaddress}:7001
 
 # Execute the commands passed to this script
 exec "$@"
