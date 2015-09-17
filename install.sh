bea2904af509b90b74d4815a870533b140cafbcf
Avesh Agarwal <avagarwa@redhat.com>
2015-09-17 09:38:13 -0400

    Removed white space.

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ chroot ${HOST} /usr/bin/docker create -p 4001:4001 -p 7001:7001 -p 2379:2379 -p 
 # Install systemd unit file for running container
 sed -e "s/TEMPLATE/${NAME}/g" /etc/systemd/system/etcd_container_template.service > ${HOST}/etc/systemd/system/etcd_container_${NAME}.service
 
-# restore selinux context 
+# restore selinux context
 chroot ${HOST} /usr/sbin/restorecon -v /etc/systemd/system/etcd_container_${NAME}.service
 
 # Enable systemd unit file
